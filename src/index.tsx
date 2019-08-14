2493ab87487ebefe6c5fec8819437364be9d0cb3
Khairul Anshar <khairul.a@saltmine.com>
2019-08-14 09:46:01 +0800

    multi reducers

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import * as ReactDOM from "react-dom";
 import { Root } from './pages/root'
 import { Provider } from './storage/provide'
 
-ReactDOM.render(<Provider><Root compiler="TypeScript" framework="React" store="Hooks (useContext & multi useReducers)"/></Provider>,
+ReactDOM.render(<Provider><Root compiler="TypeScript" framework="React" store="Hooks (useContext & multi reducers)"/></Provider>,
     document.getElementById('start'))
